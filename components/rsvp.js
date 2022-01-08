0b31030c2a2b6188b403d9fcf8c760b794d55e2a
Jon Heslop <jon@jonheslop.com>
2022-01-08 13:52:06 +0000

    Split RSVP into component

---
+++ components/rsvp.js
@@ -0,0 +1,113 @@
+import React, { Fragment, useState } from 'react';
+import Input from '../components/input';
+import TextArea from '../components/textarea';
+import Radio from '../components/radio';
+
+export default function RSVP() {
+  const [attending, setAttending] = useState(false);
+  const [pending, setPending] = useState(false);
+  const [complete, setComplete] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const payload = {
+      name: e.target.elements.name.value,
+      email: e.target.elements.email.value,
+      attending: e.target.elements.attending.value,
+    };
+
+    if (e.target.elements.attending.value === 'yes') {
+      payload.count = e.target.elements.count.value;
+      payload.dietary = e.target.elements.dietary.value;
+    }
+
+    setPending(true)
+    const response = await fetch('/api/submit', {
+      method: 'POST',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+
+    const result = await response.json();
+
+    if (response.status === 201) {
+      setComplete(true);
+      setPending(false);
+    } else {
+      setComplete(false);
+      setPending(false);
+    }
+  }
+
+  if (!complete) return (
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Input name="name" label="Name" placeholder="The Smiths" />
+        <Input
+          name="email"
+          label="Email address"
+          type="email"
+          placeholder="name@email.com"
+        />
+
+        <fieldset className="mt-4">
+          <legend className="text-xl">Are you attending?</legend>
+          <Radio
+            label="Yes"
+            name="attending"
+            value="yes"
+            onChange={() => setAttending(true)}
+          />
+          <Radio
+            label="No"
+            name="attending"
+            value="no"
+            onChange={() => setAttending(false)}
+          />
+        </fieldset>
+
+        {attending && (
+          <Fragment>
+            <Input
+              name="count"
+              classes="w-32"
+              label="Number of people"
+              inputMode="numeric"
+              pattern="[0-9]*"
+            />
+            <TextArea
+              name="dietary"
+              label="Dietary requirements"
+              placeholder="Vegan"
+            />
+          </Fragment>
+        )}
+        <button className="block py-2 px-12 text-xl focus:ring ring-hinterland/50 outline-none rounded text-white/80 placeholder-stone-300 bg-stone-400 mt-8 hover:bg-hinterland transition-colors">
+          {pending && (
+            <span className="block w-8 h-8 border-4 rounded-full border-white border-r-transparent animate-spin flex-shrink-0">
+              <span className="sr-only">Loading</span>
+            </span>
+          )}
+          {!pending && 'Submit'}
+        </button>
+      </form>
+    );
+
+  if (complete) return (
+      <div>
+        {attending && (
+          <p className="text-xl leading-normal">
+            <span className="text-4xl block mb-2">🎉</span> Thank you! We’re so
+            excited to have you.
+          </p>
+        )}
+        {!attending && (
+          <p className="text-xl leading-normal">
+            Thanks, we’re sorry you can’t make it.
+          </p>
+        )}
+      </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,7 @@
 import React, { Fragment, useState } from 'react';
-
 import Layout from '../components/layout';
-import Input from '../components/input';
-import TextArea from '../components/textarea';
-import Radio from '../components/radio';
-
+import RSVP from "../components/rsvp";
 export default function Home() {
-  const [attending, setAttending] = useState(false);
-  const [pending, setPending] = useState(false);
-  const [complete, setComplete] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const payload = {
-      name: e.target.elements.name.value,
-      email: e.target.elements.email.value,
-      attending: e.target.elements.attending.value,
-    };
-
-    if (e.target.elements.attending.value === 'yes') {
-      payload.count = e.target.elements.count.value;
-      payload.dietary = e.target.elements.dietary.value;
-    }
-
-    setPending(true)
-    const response = await fetch('/api/submit', {
-      method: 'POST',
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    });
-
-    const result = await response.json();
-
-    if (response.status === 201) {
-      setComplete(true);
-      setPending(false);
-    } else {
-      setComplete(false);
-      setPending(false);
-    }
-  }
-
   return (
     <Layout title="A wedding is occurring">
       <div>
@@ -65,74 +23,7 @@ export default function Home() {
         </p>
         <h2 className="text-xl mt-8">Please RSVP</h2>
       </div>
-      {!complete && (
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <Input name="name" label="Name" placeholder="The Smiths" />
-          <Input
-            name="email"
-            label="Email address"
-            type="email"
-            placeholder="name@email.com"
-          />
-
-          <fieldset className="mt-4">
-            <legend className="text-xl">Are you attending?</legend>
-            <Radio
-              label="Yes"
-              name="attending"
-              value="yes"
-              onChange={() => setAttending(true)}
-            />
-            <Radio
-              label="No"
-              name="attending"
-              value="no"
-              onChange={() => setAttending(false)}
-            />
-          </fieldset>
-
-          {attending && (
-            <Fragment>
-              <Input
-                name="count"
-                classes="w-32"
-                label="Number of people"
-                placeholder="2"
-                inputMode="numeric"
-                pattern="[0-9]*"
-              />
-              <TextArea
-                name="dietary"
-                label="Dietary requirements"
-                placeholder="Vegan"
-              />
-            </Fragment>
-          )}
-          <button className="block py-2 px-12 text-xl focus:ring ring-hinterland/50 outline-none rounded text-white/80 placeholder-stone-300 bg-stone-400 mt-8 hover:bg-hinterland transition-colors">
-            {pending && (
-              <span className="block w-8 h-8 border-4 rounded-full border-white border-r-transparent animate-spin flex-shrink-0">
-                <span className="sr-only">Loading</span>
-              </span>
-            )}
-            {!pending && 'Submit'}
-          </button>
-        </form>
-      )}
-      {complete && (
-        <div>
-          {attending && (
-            <p className="text-xl leading-normal">
-              <span className="text-4xl block mb-2">🎉</span> Thank you! We’re
-              so excited to have you.
-            </p>
-          )}
-          {!attending && (
-            <p className="text-xl leading-normal">
-              Thanks, we’re sorry you can’t make it.
-            </p>
-          )}
-        </div>
-      )}
+      <RSVP/>
     </Layout>
   );
 }
